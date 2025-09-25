9a15c89dccd9ef7aaa4930c368eef121e9eda9a5
Sonukumar0009 <sonukumarryder@gmail.com>
2025-09-25 16:23:09 +0530

    first commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -28,11 +28,12 @@ mongoose
   .catch((e) => {
     console.error("Failed to connect to MongoDB:", e);
   });
-
+  
+const allowedOrigins=['http://localhost:5173','https://live-polling-system-gold.vercel.app']
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "allowedOrigins",
     methods: ["GET", "POST"],
     credentials: true,
   },
